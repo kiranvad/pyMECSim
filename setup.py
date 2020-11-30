a3ef7d14bf0b1223283645001b0c011d2389d3c9
Kiran Vaddi <kiranvad@cpn-p28-07.cbls.ccr.buffalo.edu>
2020-11-30 16:31:36 -0500

    merge pip installable package from dev branch

--- setup.py
+++ setup.py
@@ -11,5 +11,11 @@ setup(name="pymecsim",
       install_requires=['numpy','scipy', 'pandas'],
       extras_require = {},
       packages=find_packages(),
-      long_description=open('Readme.md').read()
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
 )
